646b8f099ae7a6c9b9c4820b56927fd823fb283b
dhacker29 <davidhackerdvm@gmail.com>
2012-12-28 06:28:09 -0600

    derp

--- config/common.mk
+++ config/common.mk
@@ -37,9 +37,10 @@ ifeq ($(TARGET_PRODUCT),killr_mako)
 PRODUCT_COPY_FILES += \
     vendor/google/killrom/system/app/mako/GalleryGoogle.apk:$(PRODUCT_OUT)/system/app/GalleryGoogle.apk \
     vendor/google/killrom/system/app/mako/LatinIME.apk:$(PRODUCT_OUT)/system/app/LatinIME.apk \
-    vendor/google/killrom/system/app/OccamQuickOffice.apk:$(PRODUCT_OUT)/system/app/OccamQuickOffice.apk \
+    vendor/google/killrom/system/app/OccamQuickOffice.apk:$(PRODUCT_OUT)/system/app/OccamQuickOffice.apk
 else
-    vendor/google/killrom/system/app/LatinIME.apk:$(PRODUCT_OUT)/system/app/LatinIME.apk \
+PRODUCT_COPY_FILES += \
+    vendor/google/killrom/system/app/LatinIME.apk:$(PRODUCT_OUT)/system/app/LatinIME.apk
 endif
 
 ifneq ($(filter full_maguro full_toro,$(TARGET_PRODUCT)),)
